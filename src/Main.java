d5a104ffc55ada6185c7b189625a58e948a505de
mameevdi <mameevs@gmail.com>
2022-12-04 00:11:50 +0300

    Проект создан

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog*10;
+        System.out.println(frog);
+        frog = frog/3.5;
+        System.out.println(frog);
+        frog = frog+4;
+        System.out.println(frog);
+    }
+}
